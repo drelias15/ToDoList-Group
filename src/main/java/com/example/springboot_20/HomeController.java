c5adcb29774a37a66d63fe3d65311bb209b358fc
drelias15 <drelias15@gmail.com>
2018-08-14 18:12:35 -0400

    first

---
+++ src/main/java/com/example/springboot_20/HomeController.java
@@ -0,0 +1,96 @@
+package com.example.springboot_20;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.security.Principal;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    TaskRepository taskRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String showRegistrationPage(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String processRegistrationPage(
+            @Valid @ModelAttribute("user") User user,
+            BindingResult result,
+            Model model) {
+        model.addAttribute("user", user); //here
+        if (result.hasErrors()) {
+            return "registration";
+        } else {
+            userService.saveUser(user);
+            model.addAttribute("message",
+                    "User Account Successfully Created");
+        }
+        return "index";
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+//    @RequestMapping("/secure")
+//    public String secure() {
+//        return "list";
+//    }
+    @RequestMapping("/update/{username}")
+    public String userUpdate(@PathVariable("username") String username, Model model) {
+        model.addAttribute("task", taskRepository.findByUsername(username));
+        return "task";
+    }
+
+    @RequestMapping("/secure")
+    public String searchByName(Model model) {
+        String username = getUser().getUsername();
+        model.addAttribute("tasks", taskRepository.findByUsername(username));
+        return "list";
+
+    }
+    @GetMapping("/add")
+    public String addTask(Model model){
+        model.addAttribute("task", new Task());
+        return "task";
+    }
+    @PostMapping("/process2")
+    public String processForm( @ModelAttribute Task task, BindingResult result, Model model)
+    {
+        taskRepository.save(task);
+        String username = getUser().getUsername();
+        model.addAttribute("tasks", taskRepository.findByUsername(username));
+        return "list";
+    }
+
+    private User getUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentusername = authentication.getName();
+        User user = userRepository.findByUsername(currentusername);
+        return user;
+    }
+}
